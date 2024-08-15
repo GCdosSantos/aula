aed2f7aa7cefa81e04f4484f04b666d3f472e5d4
GCdosSantos <163474541+GCdosSantos@users.noreply.github.com>
2024-08-14 22:26:27 -0300

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int divisivel_porQuatro(int num){
+	if (num % 4 == 0){
+		printf("O VALOR %d E DIVISIVEL POR 4. ", valor_tres);
+	} else {
+		printf("O NUMERO %d NAO E DIVISIVEL POR 4. ", valor_tres);
+	}
+	
+	
+	
+	
+}
+int main(){
+    int valor_tres;
+    
+    printf("INSIRA UM VALOR DE 3 DIGITOS: \n");
+    scanf("%d", valor_tres);
+    
+    
+    	if(valor_tres <= 100 && >= 999){
+    		printf("O NAO TEM 3 DIGITOS. ", valor_tres);
+}
+	}
+
+
+
+
+
+
+
+
+return 0;
+}
